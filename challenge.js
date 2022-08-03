0f155ef7e4695bb32fc37c04a27cf5c7c77695b0
taiwonaf <nafiutaiwosolomon@gmail.com>
2022-08-03 12:33:24 +0100

    Remaining number 6

--- challenge.js
+++ challenge.js
@@ -3,12 +3,21 @@ document.addEventListener("DOMContentLoaded", function(){
 });
 
 function addWavyUnderline(elements) {
+  // takes element as an argument
+  elements.forEach(element=>{
+    element.style.textDecoration = 'wavy underline'
+  })
 }
 
 function displayProjects() {
 }
 
+
 function findAllUnorderedListElements() {
+
+  // return all unordered list items
+  const nodeItems = document.querySelectorAll('ul>li');
+  return nodeItems;
 }
 
 function prepareProjects() {
